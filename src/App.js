682c3d1b82d19353f3bcad2878d24f2d2d54c848
ms <15849124005@163.com>
2020-03-15 16:56:38 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React,{Component} from 'react'
-import {Switch, Route, Redirect} from 'react-router-dom'
-import Login from './pages/login/login'
-import Admin from './pages/admin/admin'
-
-
 class App extends Component{
     render(){
         return(
-           <Switch>
-             <Route path='/login' component={Login}/>
-             <Route path='/admin' component={Admin}/>
-             <Redirect to="/login"/>
-           </Switch> 
-        
+            <div>
+
+                App
+            </div>
         )
     }
 }
